4921490e060b248f5b5d1e1093ab70f20cd1b360
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2023-03-26 12:11:40 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Examen JS 2021 - Enquête</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="css/main.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="container">
+            <div id="message" class="alert alert-danger">
+                <strong>Erreur !</strong> Vous devez répondre à toutes les questions
+            </div>
+            <h3></h3>
+            <div id="questions"></div>
+            <input type="button" value="Suivant" id="next" class="btn btn-primary"/>
+            <input type="button" value="Valider" id="confirm" class="btn btn-success"/>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+        <script src="js/main.js" type="text/javascript"></script>
+    </body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,145 @@
+let maPage = 0;
+let lengthQuestionnaire = 0;
+let monSousTitre = document.querySelector('h3');
+let maLocation = document.getElementById('questions');
+let nextBtn = document.getElementById('next');
+
+// Ajax request count pages
+let xhrCount = new XMLHttpRequest;
+let urlcount = `./enquete.php?length`;
+xhrCount.open('GET', urlcount);
+xhrCount.onreadystatechange = function() {
+    if (xhrCount.readyState == 4 && xhrCount.status == 200) {
+        let monCount = JSON.parse(xhrCount.response).length;
+        lengthQuestionnaire += monCount;
+    }
+}
+xhrCount.send(null);
+
+// call the Ajax for the first time
+pageGenerator();
+
+// function to call the Ajax
+function pageGenerator() {
+    maPage++;
+    let xhrPage = new XMLHttpRequest;
+    let urlpage = `./enquete.php?page=${maPage}`;
+    xhrPage.open('GET', urlpage);
+    xhrPage.onreadystatechange = function() {
+        if (xhrPage.readyState == 4 && xhrPage.status == 200) {
+            let questionnaire = JSON.parse(xhrPage.response);
+            console.log(questionnaire);
+        
+            // understand type
+            let monType = questionnaire.type;
+
+            // Add title
+            monSousTitre.innerHTML = `Question(s) ${maPage}/${lengthQuestionnaire}`
+        
+            // Create elements of the table
+            let newTable = document.createElement('table');
+            newTable.classList.add(`Table${maPage}`);
+            maLocation.appendChild(newTable);
+
+            // Create title
+            let newTableRow1 = document.createElement('tr');
+            let newTableHead = document.createElement('th');
+            newTableHead.innerHTML = questionnaire.title;
+            newTableRow1.appendChild(newTableHead);
+            if (monType == "boolean") {
+                let Oui = document.createElement('th');
+                Oui.innerHTML = 'Oui';
+                newTableRow1.appendChild(Oui);
+                let Non = document.createElement('th');
+                Non.innerHTML = 'Non';
+                newTableRow1.appendChild(Non);
+            } else if (monType == "scale") {
+                let Ti = document.createElement('th');
+                Ti.innerHTML = "Tres Insatisfait";
+                newTableRow1.appendChild(Ti);
+                let I = document.createElement('th');
+                I.innerHTML = "Insatisfait";
+                newTableRow1.appendChild(I);
+                let N = document.createElement('th');
+                N.innerHTML = "Neutre";
+                newTableRow1.appendChild(N);
+                let S = document.createElement('th');
+                S.innerHTML = "satisfait";
+                newTableRow1.appendChild(S);
+                let Ts = document.createElement('th');
+                Ts.innerHTML = "très satisfait";
+                newTableRow1.appendChild(Ts);
+            } else if (monType == "text") {
+                let texte = document.createElement('th');
+                texte.innerHTML = "Réponse libre";
+                newTableRow1.appendChild(texte);
+            }
+            newTableRow1.classList.add(`Table${monType}`);
+            newTable.appendChild(newTableRow1);
+
+            // Create table content
+            let compteur = 1;
+            for (const key in questionnaire.questions) {
+                let newTableRow2 = document.createElement('tr');
+                let newTableDetail = document.createElement('td');
+                newTableDetail.innerHTML = questionnaire.questions[`${key}`];
+                newTableRow2.appendChild(newTableDetail);
+                if (monType == "boolean") {
+                    let Oui = document.createElement('td');
+                    Oui.innerHTML = `<input type="radio" id="Q${compteur}Oui" name="Q${compteur}" value="Oui">`;
+                    newTableRow2.appendChild(Oui);
+                    let Non = document.createElement('td');
+                    Non.innerHTML = `<input type="radio" id="Q${compteur}Non" name="Q${compteur}" value="Non">`;
+                    newTableRow2.appendChild(Non);
+                } else if (monType == "scale") {
+                    let Ti = document.createElement('td');
+                    Ti.innerHTML = `<input type="radio" id="Q${compteur}TI" name="Q${compteur}" value="tresInsatisfait">`;
+                    newTableRow2.appendChild(Ti);
+                    let I = document.createElement('td');
+                    I.innerHTML = `<input type="radio" id="Q${compteur}I" name="Q${compteur}" value="insatisfait">`;
+                    newTableRow2.appendChild(I);
+                    let N = document.createElement('td');
+                    N.innerHTML = `<input type="radio" id="Q${compteur}N" name="Q${compteur}" value="Neutre">`;
+                    newTableRow2.appendChild(N);
+                    let S = document.createElement('td');
+                    S.innerHTML = `<input type="radio" id="Q${compteur}S" name="Q${compteur}" value="Satisfait">`;
+                    newTableRow2.appendChild(S);
+                    let Ts = document.createElement('td');
+                    Ts.innerHTML = `<input type="radio" id="Q${compteur}Ts" name="Q${compteur}" value="tresSatisfait">`;
+                    newTableRow2.appendChild(Ts);
+                } else if (monType == "text") {
+                    let texte = document.createElement('td');
+                    texte.innerHTML = `<input type="text" id="Q${compteur}texte" name="Q${compteur}" value="text" minlength="4" maxlength="8" size="10">`;
+                    newTableRow2.appendChild(texte);
+                }
+                newTableRow2.classList.add(`Table${monType}`);
+                newTable.appendChild(newTableRow2);
+                compteur += 1;
+            }
+        }
+    }
+    xhrPage.send(null);
+}
+
+// Gestion de l'alerte
+/*function checkAlert() {
+    let alerte = document.querySelectorAll('input');
+    console.log(alerte);
+    for(i in alerte){
+        console.log(i);
+        if (i.checked) {  
+            console.log("c'est checked");
+        }
+    }
+};*/
+
+// Bouton "next""
+let btnNext = document.getElementById("next");
+let toBeHidden = document.querySelector(`.Table${maPage}`);
+console.log(toBeHidden);
+//toBeHidden.classList.add("hidden");
+btnNext.addEventListener("click", pageGenerator);
+//btnNext.addEventListener("click", checkAlert);
+
+
+
